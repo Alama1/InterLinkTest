eb6666681ff1a5e36f0d59443f81bbfd77f6cf99
Alama1 <oldpaladin7@gmail.com>
2021-03-12 15:58:39 +0200

    It's working, but brackets 1.0

--- src/InterLink.java
+++ src/InterLink.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class InterLink {
 
@@ -10,31 +7,48 @@ public class InterLink {
     public static void main(String[] args) throws IOException {
         //Путь до файлу
         String fileRead = "resources\\acme_worksheet.csv";
-        //todo writer
-        Path fileWrite = FileSystems.getDefault().getPath("resources", "acme_worksheet_new.csv");
-
+        FileWriter pw = new FileWriter("resources\\out\\data.csv", true);
         BufferedReader reader = null;
-        BufferedReader writer = null;
-        String oneLine = "";
-        String line = "";
-
+        String line;
+        ArrayList<String> employee = new ArrayList<>();
+        ArrayList<String> dates = new ArrayList<>();
+        ArrayList<String> hours = new ArrayList<>();
         try {
             reader = new BufferedReader(new FileReader(fileRead));
 
             while((line = reader.readLine()) != null){
+                DateFormater dateFormater = new DateFormater();
                 String[] row = line.split(",");
-                for(String index : row){
-                    System.out.printf("%-20s", index);
+                employee.add(row[0]);
+                String[] date = row[1].split(" ");
 
+                if(date[0].length() != 3){
+                    dates.add(date[0]);
                 }
-                //Переносы
-                System.out.println();
+                else{
+                    String finalDate = "";
+                    finalDate += (date[2]);
+                    finalDate += ("-");
+                    finalDate += (dateFormater.DateForm(date[0]));
+                    finalDate += ("-");
+                    finalDate += (date[1]);
+                    dates.add(finalDate);
+                }
+
+                hours.add(row[2]);
+
             }
+            pw.write(employee.toString());
+            pw.write("\n");
+            pw.write(dates.toString());
+            pw.write("\n");
+            pw.write(hours.toString());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             reader.close();
+            pw.flush();
+            pw.close();
         }
-
     }
 }
